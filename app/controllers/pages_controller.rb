3a8a1201ad26acfddb9f8786035c14cf7d7dbe94
figmentbml <beth.jaswa@gmail.com>
2014-10-15 13:40:23 -0600

    refactored quotes into the controller and put old pics back in index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
 
+  def index
+    @quotes = [
+      ["Failure is not an option. Everyone has to succeed", "Arnold"],
+      ["Your time is limited, so don't waste it living someone else's life.", "Steve Jobs"],
+      ["The present is surrendered to the past and the future.", "Walker Percy"]
+        ]
+  end
+
 end
