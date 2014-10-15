8cd6c34195a83ea7f05a58f145a366ef43f15e59
figmentbml <beth.jaswa@gmail.com>
2014-10-15 14:39:07 -0600

    added FAQ page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,5 +17,21 @@ class PagesController < ApplicationController
 
   end
 
+  def faq
+      faq1 = Faq.new
+      faq1.question = "What is gCamp?"
+      faq1.answer = "gCamp is an awesome tool that will change your life."
+
+      faq2 = Faq.new
+      faq2.question = "How do I join gCamp?"
+      faq2.answer = "Right now gCamp is still in production, so you can't join."
+
+      faq3 = Faq.new
+      faq3.question = "When will gCamp be finished?"
+      faq3.answer = "gCamp is a work in progress, but should be fully functional by Decemeber 2014.  Functional, but our developers are going to continue to improve the site for the foreseeable future."
+
+      @faqs = [faq1, faq2, faq3]
+      
+    end
 
 end
