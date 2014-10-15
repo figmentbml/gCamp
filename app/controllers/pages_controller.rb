0057c6dc98bf555b6da7ee91ba2f740962c95597
figmentbml <beth.jaswa@gmail.com>
2014-10-15 14:11:18 -0600

    added FAQ page and refactor quotes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,21 @@
 class PagesController < ApplicationController
 
   def index
-    @quotes = [
-      ["Failure is not an option. Everyone has to succeed", "Arnold"],
-      ["Your time is limited, so don't waste it living someone else's life.", "Steve Jobs"],
-      ["The present is surrendered to the past and the future.", "Walker Percy"]
-        ]
+        quote1 = Quote.new
+        quote1.quote = "Failure is not an option. Everyone has to succeed"
+        quote1.author = "Arnold"
+
+        quote2 = Quote.new
+        quote2.quote = "Your time is limited, so don't waste it living someone else's life."
+        quote2.author = "Steve Jobs"
+
+        quote3 = Quote.new
+        quote3.quote = "The present is surrendered to the past and the future."
+        quote3.author = "Walker Percy"
+
+        @quotes = [quote1, quote2, quote3]
+
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root "pages#index"
   get "/about" => "pages#about", as: :about
   get "/terms" => "pages#terms", as: :terms
-  
+  get "/faqs#top" => "pages#faq", as: :faq
+
 end
 
 
