2318cc2ae1d0abc93ed570d15d6fbab4920b4bbf
figmentbml <beth.jaswa@gmail.com>
2014-10-24 15:05:02 -0600

    new migration to fix incomplete tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021225307) do
+ActiveRecord::Schema.define(version: 20141024205456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
