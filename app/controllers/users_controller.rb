3e7ca7d8674c687568185cba393896168816c98a
figmentbml <beth.jaswa@gmail.com>
2014-10-27 14:18:22 -0600

    added users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path, notice: 'Event was successfully created.'
+  end
+
+private
+  def user_params
+    params.required(:user).permit(:first_name, :last_name, :email)
+  end
+
+end
