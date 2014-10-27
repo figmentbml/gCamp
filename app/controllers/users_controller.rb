5b561cb86b09290dc9c3a794c8f66d75f48a6032
figmentbml <beth.jaswa@gmail.com>
2014-10-27 15:07:51 -0600

    updated users and new users page to have alert and better formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
-    redirect_to users_path, notice: 'Event was successfully created.'
+    redirect_to users_path, notice: 'User was successfully created.'
   end
 
 private
