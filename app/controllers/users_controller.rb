8fa099dea6ba90ceb2b1d32ade6365512170ec69
figmentbml <beth.jaswa@gmail.com>
2014-10-29 15:48:12 -0600

    formated delete button for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,13 @@ class UsersController < ApplicationController
     redirect_to users_path, notice: "User was successfully updated."
   end
 
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "User was successfully deleted."
+  end
 
-private
+
+  private
   def set_user
     @user = User.find(params[:id])
   end
