a98d8be621242544178f1e91ab1ebf3051fd03e9
figmentbml <beth.jaswa@gmail.com>
2014-10-30 13:53:09 -0600

    added project edit page and edit/delete functions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,10 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
   def new
     @project = Project.new
   end
@@ -11,16 +15,24 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
     @project.save
-    redirect_to projects_path, notice: "Project was successfully created."
+    redirect_to project_path(@project), notice: "Project was successfully created."
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    @project.save
+    redirect_to projects_path, notice: "Project was successfully updated."
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project was successfully deleted."
   end
 
 private
