0bbc109208cc55c62fe2008df1c7f3e36cb2c9c5
figmentbml <beth.jaswa@gmail.com>
2014-11-12 13:12:36 -0700

    updated model/task_spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,13 +11,6 @@ describe "Task" do
     expect(task.valid?).to be(true)
   end
 
-  it "verifies entry of only description" do
-    task = Task.new
-    expect(task.valid?).to be(false)
-    task.description = "Test"
-    expect(task.valid?).to be(true)
-  end
-
   it "verifies can't have old date when creating" do
     task = Task.new
     expect(task.valid?).to be(false)
@@ -25,8 +18,24 @@ describe "Task" do
     expect(task.valid?).to be(true)
     task.due_date = Date.today-7
     expect(task.valid?).to be(false)
+    expect(task.errors[:due_date].present?).to eq(true)
+
     task.due_date = Date.today+7
     expect(task.valid?).to be(true)
+    expect(task.errors[:due_date].present?).to eq(false)
   end
+
+  it "verifies can have old date when updating" do
+    task = Task.create!(description: "Test")
+    expect(task.errors[:description].present?).to eq(false)
+    task.due_date = Date.today-7
+    expect(task.valid?).to be(true)
+    expect(task.errors[:due_date].present?).to eq(false)
+
+    task.due_date = Date.today+7
+    expect(task.valid?).to be(true)
+    expect(task.errors[:due_date].present?).to eq(false)
+  end
+
 
 end
