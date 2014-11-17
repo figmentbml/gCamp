6d492431db4a531aafab65d3b108c2e14fdb7e63
figmentbml <beth.jaswa@gmail.com>
2014-11-17 16:10:43 -0700

    updated task tests to reflect nesting

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 feature "Tasks" do
+  before do
+    Project.create!(
+      name: "dogs!",
+    )
+  end
 
   scenario "Create Task" do
-    visit root_path
-    click_on "Tasks"
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
     click_on "Create Task"
     fill_in "Description", with: "test"
     fill_in "Due date", with: "09/20/2014"
@@ -14,8 +20,9 @@ feature "Tasks" do
   end
 
   scenario "Attempt to create task w/o description" do
-    visit root_path
-    click_on "Tasks"
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
     click_on "Create Task"
     fill_in "Due date", with: "09/20/2014"
     click_button "Create Task"
@@ -24,8 +31,9 @@ feature "Tasks" do
   end
 
   scenario "Attempt to create task w/o due date" do
-    visit root_path
-    click_on "Tasks"
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
     click_on "Create Task"
     fill_in "Description", with: "test"
     click_button "Create Task"
@@ -33,9 +41,10 @@ feature "Tasks" do
     expect(page).to have_content("Task was successfully created.")
   end
 
-  scenario "Create Task and edit from show page" do
-    visit root_path
-    click_on "Tasks"
+  scenario "Create Task and edit from index page" do
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
     click_on "Create Task"
     fill_in "Description", with: "puppies"
     fill_in "Due date", with: "09/20/2014"
@@ -43,17 +52,18 @@ feature "Tasks" do
     expect(page).to have_content("puppies")
     expect(page).to have_content("Task was successfully created.")
     click_on "Edit"
+    expect(page).to have_content("Edit task")
     fill_in "Description", with: "testy"
-    check "Complete"
     click_button "Update Task"
     expect(page).to have_content("testy")
     expect(page).to have_no_content("puppies")
-    expect(page).to have_content("Complete: true")
+    expect(page).to have_content("Task was successfully updated.")
   end
 
-  scenario "Can't create task with old date from show page and then update with old date" do
-    visit root_path
-    click_on "Tasks"
+  scenario "Can't create task with old date from index page and then update with old date" do
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
     click_on "Create Task"
     fill_in "Description", with: "puppies"
     fill_in "Due date", with: Date.today-7
@@ -64,7 +74,6 @@ feature "Tasks" do
     click_button "Create Task"
     click_on "Edit"
     fill_in "Due date", with: Date.today-5
-    check "Complete"
     click_button "Update Task"
     expect(page).to have_content("puppies")
     expect(page).to have_no_content("Due date can't be in the past")
@@ -72,55 +81,48 @@ feature "Tasks" do
   end
 
 
-  scenario "Create Task and Edit from index page" do
-    Task.create!(
-      description: "puppies",
-      due_date: "09/20/2014",
-    )
-    visit tasks_path
+  scenario "Create Task and Edit from show page" do
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
+    click_on "Create Task"
+    fill_in "Description", with: "puppies"
+    fill_in "Due date", with: "09/20/2014"
+    click_button "Create Task"
     expect(page).to have_content("puppies")
+    expect(page).to have_content("Task was successfully created")
+    click_on "Show"
     click_on "Edit"
     fill_in "Description", with: "kittens"
     click_button "Update Task"
     expect(page).to have_no_content("puppies")
     expect(page).to have_content("kittens")
     expect(page).to have_content("Task was successfully updated.")
-    click_on "Back"
-    expect(page).to have_content("Tasks")
   end
 
   scenario "Delete task from index page" do
-    Task.create!(
-      description: "puppies",
-      due_date: "09/20/2014",
-    )
-    visit tasks_path
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
+    click_on "Create Task"
+    fill_in "Description", with: "puppies"
+    fill_in "Due date", with: "09/20/2014"
+    click_button "Create Task"
     expect(page).to have_content("puppies")
+    expect(page).to have_content("Task was successfully created")
     click_on "Destroy"
     expect(page).to have_no_content("puppies")
-  end
-
-  scenario "Show all tasks from index" do
-    Task.create!(
-      description: "puppies",
-      due_date: "09/20/2014",
-    )
-    visit tasks_path
-    click_on "Edit"
-    check "Complete"
-    click_button "Update Task"
-    click_on "Back"
-    click_on "All"
-    expect(page).to have_content("puppies")
-    expect(page).to have_content("true")
+    expect(page).to have_content("Task was successfully destroyed")
   end
 
   scenario "Show all tasks from index, then incomplete tasks" do
-    Task.create!(
-      description: "puppies",
-      due_date: "09/20/2014",
-    )
-    visit tasks_path
+    visit projects_path
+    click_on "dogs!"
+    click_on "0 Tasks"
+    click_on "Create Task"
+    fill_in "Description", with: "puppies"
+    fill_in "Due date", with: "09/20/2014"
+    click_button "Create Task"
     click_on "All"
     expect(page).to have_content("puppies")
     expect(page).to have_content("false")
