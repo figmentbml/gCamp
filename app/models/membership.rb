c8a67bdf7d9c2acd7334921062cc8f93055dc7ff
figmentbml <beth.jaswa@gmail.com>
2014-11-19 11:37:10 -0700

    updated validations on membership to only have one membership per user per project

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,7 +3,8 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  validates :user_id, presence: :true, uniqueness: :true
+  validates :user_id, presence: :true, uniqueness: {scope: :project_id,
+    message: "should only have one membership per project" }
   validates :role, presence: :true
 
 end
