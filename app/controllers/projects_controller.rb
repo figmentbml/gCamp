acdb89153b92f6e104160541419157ab1152a784
figmentbml <beth.jaswa@gmail.com>
2014-12-01 15:32:54 -0700

    new project redirects to project tasks index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,7 +15,7 @@ class ProjectsController < InternalController
   def create
     @project = Project.new(project_params)
     if @project.save
-      redirect_to project_path(@project), notice: "Project was successfully created."
+      redirect_to project_tasks_path(@project), notice: "Project was successfully created."
     else
       render :new
     end
