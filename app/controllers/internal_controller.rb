6cead7d578c7e05e83f7ab6e65f27cb4331d7632
figmentbml <beth.jaswa@gmail.com>
2014-12-01 15:18:16 -0700

    added drop down menu to internal nav bar

--- app/controllers/internal_controller.rb
+++ app/controllers/internal_controller.rb
@@ -1,11 +1,16 @@
 class InternalController < ApplicationController
   before_action :ensure_logged_in_user
+  before_action :projects
 
   private
   def ensure_logged_in_user
     unless current_user
       redirect_to signin_path, notice: "You must be logged in to access that action"
     end
+  end
+
+  def projects
+    @projects = Project.all
   end
 
 end
