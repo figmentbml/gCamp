0904b9ee34d145ebc4baa14b7467c799db6070d3
figmentbml <beth.jaswa@gmail.com>
2014-12-17 10:40:25 -0700

    added page header helper. included in projects and tasks form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def page_header(title, &block)
+    content_for(:title, title)
+    html = '<div class = "page-header">'.html_safe
+    if block
+      html += '<div class = "pull-right">'.html_safe
+      html += capture(&block)
+      html += '</div>'.html_safe
+    end
+    html += '<h1>'.html_safe
+    html += title
+    html += '</h1>'.html_safe
+    html += '</div>'.html_safe
+    html
+  end
+
 end
