b0f1136ec3f880077809f953c4d6f81d4aedca56
figmentbml <beth.jaswa@gmail.com>
2014-12-17 12:53:54 -0700

    fixed home page and got the name on stories show page

--- app/models/tracker_api.rb
+++ app/models/tracker_api.rb
@@ -15,6 +15,16 @@ class TrackerAPI
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def project(project_id, token)
+    return [ ] if token.nil? #precondition
+    response = @conn.get do |req|
+      req.url "/services/v5/projects/#{project_id}"
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['X-TrackerToken'] = token
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def stories(tracker_id, token)
     return [ ] if token.nil?
 
